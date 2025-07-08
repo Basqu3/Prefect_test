46784ccc5ff849827f5161630b89d3d7e5a5ed89
Basqu3 <jpatxins@gmail.com>
2025-07-08 15:26:50 -0600

    first

---
+++ hello.py
@@ -0,0 +1,17 @@
+from prefect import flow, tags
+import random
+
+@flow(log_prints=True)
+def hello(name: str = "World") -> None:
+    print(f"Hello, {name}!")
+    if random.random() < 0.5:
+        raise ValueError("An error occurred in the flow!")
+    return f"Flow completed successfully with name: {name}"
+
+if __name__ == "__main__":
+    with tags("example1"):
+        hello("Prefect User")
+    with tags("example2"):
+        hello("Prefect User 2")
+    with tags("example3"):
+        hello("Prefect User 3")
